6111dbe3f9487daf366f6c6871f31c6350ea61dd
andrewhau1 <andrewhau@gmail.com>
2014-07-09 18:50:08 -0400

    Create user resource and user form and profile pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def new
+    	@user = User.new
+  	end
+
+  	def create
+    	@user = User.new(user_params)
+
+ 	   if @user.save
+    	  redirect_to @user, notice: "Thank you for signing up for Ribbit!"
+    	else
+      		render 'new'
+    	end
+  	end
+
+	def show
+    	@user = User.find(params[:id])
+  	end
+
+  	private
+  	def user_params
+    	params.require(:user).permit(:email, :name, :password, :password_confirmation, :username, :avatar_url)
+  	end
+end
